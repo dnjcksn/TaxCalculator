0c478dfae77b57388716db48a598e5e5d8f0d802
dnjcksn <danielajackson@hotmail.co.uk>
2026-06-29 22:02:14 +0100

    Create tax summary

--- TaxCalculator.Application/TaxCalculatorService.cs
+++ TaxCalculator.Application/TaxCalculatorService.cs
@@ -5,6 +5,10 @@ using TaxCalculator.Domain;
 
 public class TaxCalculatorService : ITaxCalculatorService
 {
+
+    private const int MonthsPerYear = 12;
+    private const int CurrencyDecimalPlaces = 2;
+
     private readonly ITaxBandRepository _taxBandRepository;
 
     public TaxCalculatorService(ITaxBandRepository taxBandRepository)
@@ -12,8 +16,29 @@ public class TaxCalculatorService : ITaxCalculatorService
         _taxBandRepository = taxBandRepository;
     }
 
-    public Task<TaxSummary> Calculate(decimal grossAnnualSalary)
+    public async Task<TaxSummary> Calculate(decimal grossAnnualSalary)
     {
-        throw new NotImplementedException();
+        var taxBands = await _taxBandRepository.GetAllAsync();
+        var annualTax = CalculateAnnualTax(grossAnnualSalary, taxBands);
+        var netAnnualSalary = grossAnnualSalary - annualTax;
+        decimal grossMonthlySalary = Math.Round(grossAnnualSalary / MonthsPerYear, CurrencyDecimalPlaces);
+
+        decimal netMonthlySalary = Math.Round(netAnnualSalary / MonthsPerYear, CurrencyDecimalPlaces);
+        decimal monthlyTax = Math.Round(annualTax / MonthsPerYear, CurrencyDecimalPlaces);
+
+        return new TaxSummary
+        {
+            GrossAnnualSalary = grossAnnualSalary,
+            GrossMonthlySalary = grossMonthlySalary,
+            NetAnnualSalary = netAnnualSalary,
+            NetMonthlySalary = netMonthlySalary,
+            AnnualTax = annualTax,
+            MonthlyTax = monthlyTax,
+        };
+    }
+
+    private decimal CalculateAnnualTax(decimal grossAnnualSalary, List<TaxBand> taxBands)
+    {
+        return 0;
     }
 }
