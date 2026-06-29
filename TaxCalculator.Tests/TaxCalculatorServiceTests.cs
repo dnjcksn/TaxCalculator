7f4c58e4bcb055eecee10652a140700ef33aa3e7
dnjcksn <danielajackson@hotmail.co.uk>
2026-06-29 21:53:37 +0100

    Add test cases

--- TaxCalculator.Tests/TaxCalculatorServiceTests.cs
+++ TaxCalculator.Tests/TaxCalculatorServiceTests.cs
@@ -23,4 +23,40 @@ public class TaxCalculatorServiceTests
 
         _service = new TaxCalculatorService(mockTaxBandRepository.Object);
     }
+
+    [Fact]
+    public async Task Calculate_Salary10000_Returns1000()
+    {
+        var result = await _service.Calculate(10000);
+        result.GrossAnnualSalary.Should().Be(10000);
+        result.GrossMonthlySalary.Should().Be(833.33m);
+        result.NetAnnualSalary.Should().Be(9000);
+        result.NetMonthlySalary.Should().Be(750.00m);
+        result.AnnualTax.Should().Be(1000);
+        result.MonthlyTax.Should().Be(83.33m);
+    }
+
+    [Fact]
+    public async Task Calculate_Salary40000_Returns11000()
+    {
+        var result = await _service.Calculate(40000);
+        result.GrossAnnualSalary.Should().Be(40000);
+        result.GrossMonthlySalary.Should().Be(3333.33m);
+        result.NetAnnualSalary.Should().Be(29000);
+        result.NetMonthlySalary.Should().Be(2416.67m);
+        result.AnnualTax.Should().Be(11000);
+        result.MonthlyTax.Should().Be(916.67m);
+    }
+
+    [Fact]
+    public async Task Calculate_Salary1000_Returns0()
+    {
+        var result = await _service.Calculate(1000);
+        result.GrossAnnualSalary.Should().Be(1000);
+        result.GrossMonthlySalary.Should().Be(83.33m);
+        result.NetAnnualSalary.Should().Be(1000);
+        result.NetMonthlySalary.Should().Be(83.33m);
+        result.AnnualTax.Should().Be(0);
+        result.MonthlyTax.Should().Be(0);
+    }
 }
