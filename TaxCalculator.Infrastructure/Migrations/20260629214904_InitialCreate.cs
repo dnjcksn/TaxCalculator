b8cb09c2507426d8b574311da8916b701d0dfdb8
dnjcksn <danielajackson@hotmail.co.uk>
2026-06-29 22:50:24 +0100

    Add EF Core migration

---
+++ TaxCalculator.Infrastructure/Migrations/20260629214904_InitialCreate.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace TaxCalculator.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TaxBands",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", maxLength: 100, nullable: false),
+                    LowerLimit = table.Column<int>(type: "INTEGER", nullable: false),
+                    TaxRate = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TaxBands", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "TaxBands",
+                columns: new[] { "Id", "LowerLimit", "Name", "TaxRate" },
+                values: new object[,]
+                {
+                    { 1, 0, "Band A", 0 },
+                    { 2, 5000, "Band B", 20 },
+                    { 3, 20000, "Band C", 40 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TaxBands");
+        }
+    }
+}
