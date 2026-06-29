ae623fd0c11f2a1c4a853d042520e545f03a96fd
dnjcksn <danielajackson@hotmail.co.uk>
2026-06-29 22:16:20 +0100

    Implement service logic

--- TaxCalculator.Application/TaxCalculatorService.cs
+++ TaxCalculator.Application/TaxCalculatorService.cs
@@ -39,6 +39,75 @@ public class TaxCalculatorService : ITaxCalculatorService
 
     private decimal CalculateAnnualTax(decimal grossAnnualSalary, List<TaxBand> taxBands)
     {
-        return 0;
+        var annualTax = 0m;
+        var rangedTaxBands = ToRangedTaxBands(taxBands);
+
+        foreach (RangedTaxBand rangedTaxBand in rangedTaxBands)
+        {
+            if (grossAnnualSalary > rangedTaxBand.TaxBand.LowerLimit)
+            {
+                var upperLimit = GetUpperLimit(rangedTaxBand, grossAnnualSalary);
+                var taxBandAmount = upperLimit - rangedTaxBand.TaxBand.LowerLimit;
+                var taxBandPercentage = rangedTaxBand.TaxBand.TaxRate / 100m;
+                annualTax += taxBandAmount * taxBandPercentage;
+            }
+        }
+
+        return annualTax;
+    }
+
+    private IEnumerable<RangedTaxBand> ToRangedTaxBands(List<TaxBand> taxBands)
+    {
+        var rangedTaxBands = new List<RangedTaxBand>();
+
+        var orderedTaxBands = OrderTaxBands(taxBands);
+        var noOfTaxBands = orderedTaxBands.Count;
+
+        for (int taxBandIndex = 0; taxBandIndex < noOfTaxBands; taxBandIndex++)
+        {
+            var taxBand = orderedTaxBands[taxBandIndex];
+            var nextTaxBandIndex = taxBandIndex + 1;
+
+            var upperLimit = nextTaxBandIndex < noOfTaxBands
+                ? orderedTaxBands[nextTaxBandIndex].LowerLimit
+                : (int?)null;
+
+            rangedTaxBands.Add(new RangedTaxBand
+            {
+                TaxBand = taxBand,
+                UpperLimit = upperLimit,
+            }
+            );
+        }
+
+        return rangedTaxBands;
+    }
+
+    private List<TaxBand> OrderTaxBands(List<TaxBand> taxBands)
+    {
+        return taxBands.OrderBy(b => b.LowerLimit).ToList();
+    }
+
+    private decimal GetUpperLimit(RangedTaxBand rangedTaxBand, decimal grossAnnualSalary)
+    {
+        if (rangedTaxBand.UpperLimit is null)
+        {
+            return grossAnnualSalary;
+        }
+        if (rangedTaxBand.UpperLimit > grossAnnualSalary)
+        {
+            return grossAnnualSalary;
+        }
+        return rangedTaxBand.UpperLimit.Value;
+    }
+
+    /// <summary>
+    /// A tax band with an upper limit.
+    /// Used to temporarily store the upper limit which aids in tax calculations.
+    /// </summary>
+    private class RangedTaxBand
+    {
+        public required TaxBand TaxBand { get; set; }
+        public int? UpperLimit { get; set; }
     }
 }
